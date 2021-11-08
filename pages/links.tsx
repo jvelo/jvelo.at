304924200d94ef99b3d0aeb35bc27337eb80e5a5
Jerome Velociter <jerome@velociter.fr>
2021-11-08 21:10:41 +0100

    Planetscale integration using mysql2

---
+++ pages/links.tsx
@@ -0,0 +1,46 @@
+import SiteLayout from "../components/SiteLayout";
+import Head from "next/head";
+import React from "react";
+import { GetServerSideProps } from "next";
+import mysql from "mysql2";
+
+const conn = mysql.createConnection(process.env.DATABASE_URL);
+
+type Props = {
+  links: [
+    {
+      slug: string;
+      link: string;
+    }
+  ];
+};
+
+const LinksPage: React.FC<Props> = ({ links }) => {
+  return (
+    <SiteLayout>
+      <Head>
+        <title>Links</title>
+      </Head>
+
+      <h1>Links</h1>
+      <ul>
+        {links.map((link) => (
+          <li key={link.slug}>
+            <a href={link.link}>{link.slug}</a>
+          </li>
+        ))}
+      </ul>
+    </SiteLayout>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const [links] = await conn.promise().query("select * from link");
+  return {
+    props: {
+      links,
+    },
+  };
+};
+
+export default LinksPage;
