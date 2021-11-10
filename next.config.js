14184848095e69a2fe548ef12cfb347b89f465a3
Jerome Velociter <jerome@velociter.fr>
2021-11-10 09:07:00 +0100

    Fix next env mapping

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withMDX = require("@next/mdx")();
 module.exports = withMDX({
   env: {
     PRISMIC_ENDPOINT: process.env.PRISMIC_ENDPOINT,
-    DATABASE_URL: process.env.PRISMIC_ENDPOINT,
+    DATABASE_URL: process.env.DATABASE_URL,
   },
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
 });
