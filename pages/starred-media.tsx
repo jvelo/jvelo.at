fcd7310ff074d87005931a4b5cd707ea6ecca9f8
Jerome Velociter <jerome@velociter.fr>
2021-11-17 15:19:09 +0100

    Initial support for videos

--- pages/starred-media.tsx
+++ pages/starred-media.tsx
@@ -4,7 +4,7 @@
 
 import SiteLayout from "../components/SiteLayout";
 import Head from "next/head";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { twitter_like } from "@prisma/client";
 import { Button } from "../components/Button/Button";
 import useSWRInfinite from "swr/infinite";
@@ -12,10 +12,11 @@ import { GetServerSideProps } from "next";
 import absoluteUrl from "next-absolute-url";
 import Masonry from "react-masonry-css";
 import Image from "next/image";
+import { MediaEntity, Size, Status } from "twitter-d";
+import { VideoInfo } from "twitter-d/types/video_info";
 
-type Tweet = Pick<twitter_like, "text" | "created_at" | "extended_entities"> & {
-  id: string;
-};
+type Tweet = Pick<twitter_like, "id" | "text" | "created_at"> &
+  Pick<Status, "extended_entities">;
 
 type Props = {
   fallback: Data[];
@@ -27,6 +28,92 @@ type Data = {
 };
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
+
+const Video = (props: { video: VideoInfo; size: Size }) => {
+  const ref = useRef<HTMLVideoElement>();
+  const [playing, setPlaying] = useState(false);
+  const play = () => {
+    if (ref.current.paused) {
+      ref.current.play();
+      setPlaying(true);
+    } else {
+      ref.current.pause();
+      setPlaying(false);
+    }
+  };
+
+  return (
+    <div className={`video-player ${playing ? "playing" : ""}`}>
+      <div className={"play-button"} onClick={play}>
+        ▶
+      </div>
+      <video ref={ref} width={props.size.w} height={props.size.h}>
+        {props.video.variants.map((variant) => (
+          <source
+            key={variant.url}
+            src={variant.url}
+            type={variant.content_type}
+          />
+        ))}
+      </video>
+      <style jsx>{`
+        video {
+          width: 100%;
+          height: auto;
+        }
+
+        .play-button {
+          cursor: pointer;
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          width: 40px;
+          text-align: center;
+          transform: translate(-50%, -50%);
+        }
+
+        .playing .play-button {
+          display: none;
+        }
+
+        .play-button:before {
+          position: absolute;
+          top: -7px;
+          left: -4px;
+          width: 40px;
+          height: 40px;
+          border-radius: 50%;
+          border: 2px solid black;
+          content: "";
+        }
+      `}</style>
+    </div>
+  );
+};
+
+const Media = (props: { media: MediaEntity }) => {
+  const isVideo = !!props.media.video_info;
+  return (
+    <div className={`media ${isVideo ? "video" : ""}`}>
+      {!isVideo && (
+        <Image
+          width={props.media.sizes.medium.w}
+          height={props.media.sizes.medium.h}
+          src={props.media.media_url_https}
+        />
+      )}
+      {isVideo && (
+        <Video video={props.media.video_info} size={props.media.sizes.medium} />
+      )}
+
+      <style jsx>{`
+        .media {
+          position: relative;
+        }
+      `}</style>
+    </div>
+  );
+};
 
 const FavoriteMediaEntities: React.FC<Props> = ({ fallback }) => {
   const { data, size, setSize } = useSWRInfinite<Data>(
@@ -51,7 +138,7 @@ const FavoriteMediaEntities: React.FC<Props> = ({ fallback }) => {
         <title>Starred media entities</title>
       </Head>
 
-      <h1>Starred media entities</h1>
+      <h1>✯ media entities</h1>
       <Masonry
         breakpointCols={breakpoints}
         className="media-grid"
@@ -61,11 +148,7 @@ const FavoriteMediaEntities: React.FC<Props> = ({ fallback }) => {
           const media = like.extended_entities["media"][0];
           return (
             <div key={like.created_at?.toString()}>
-              <Image
-                width={media.sizes.medium.w}
-                height={media.sizes.medium.h}
-                src={media.media_url_https}
-              />
+              <Media media={media} />
             </div>
           );
         })}
